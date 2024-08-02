dfa8230e32c0f664f2781291706658359c3cbbee
Gaboo <jlopezgab25@gmail.com>
2024-08-02 21:24:13 +0800

    Fixed Animation

---
+++ assets/js/loading.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loader--con");
+  const introSection = document.querySelector(".intro--section");
+  const pTag = document.querySelector(".intro--section p");
+  loader.classList.add("active");
+  loader.remove();
+  introSection.classList.add("animate__backInDown");
+  setTimeout(function () {
+    pTag.classList.remove("hidden");
+  }, 2000);
+  pTag.classList.add("animate__backInUp");
+});
