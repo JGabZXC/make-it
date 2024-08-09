ea80191e321af2f9e1db108b3fe67a080babb80c
Gaboo <jlopezgab25@gmail.com>
2024-08-09 17:51:01 +0800

    Updated About and Added Web Icon

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,12 +1,11 @@
 window.addEventListener("load", function () {
   const loader = document.querySelector(".loader--con");
   const introSection = document.querySelector(".intro--section");
-  const pTag = document.querySelector(".intro--section p");
+  const pTag = document.querySelector(".intro--section .animate__animated");
   loader.classList.add("active");
   loader.remove();
   introSection.classList.add("animate__backInDown");
   setTimeout(function () {
-    // pTag.classList.remove("hidden");
     pTag.classList.add("animate__pulse");
   }, 1500);
 });
